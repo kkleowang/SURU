f9c5840cc10e01b9df2a83bbc9a3e3b8bec7cdb6
Leownag <123>
2022-04-11 13:51:05 +0800

    testing gesture.

--- SURU_Leo/AppDelegate.swift
+++ SURU_Leo/AppDelegate.swift
@@ -6,11 +6,14 @@
 //
 
 import UIKit
+import Firebase
+import FirebaseFirestore
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        FirebaseApp.configure()
         return true
     }
 
--- SURU_Leo/ViewController.swift
+++ SURU_Leo/ViewController.swift
@@ -6,10 +6,52 @@
 //
 
 import UIKit
+import Lottie
+import Kingfisher
+import Firebase
+import FirebaseFirestoreSwift
 
 class ViewController: UIViewController {
+    let mask = CALayer()
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        self.view.backgroundColor = .black
+        let animationView = AnimationView(name: "wave")
+        animationView.center = self.view.center
+        animationView.contentMode = .scaleAspectFill
+        mask.backgroundColor = UIColor.black.cgColor
+        mask.frame = CGRect(x: 50, y: 50, width: 150, height: 800)
+        //        mask.cornerRadius =  mask.frame.width/2
+        animationView.layer.mask = mask
+        view.addSubview(animationView)
+        
+        animationView.play(completion: nil)
+        setGesture(importView: animationView)
+        //        let doc: DocumentReference
+        
+        
+        //        self.view.addSubview(processedImage)
     }
+    func setGesture(importView: UIView) {
+        let pan = UIPanGestureRecognizer(target: self, action: #selector(handlePan(sender:)))
+        importView.addGestureRecognizer(pan)
+    }
+    @objc func handlePan(sender: UIPanGestureRecognizer) {
+        let controledView = sender.view
+        let translation = sender.translation(in: view)
+        switch sender.state {
+        case .began, .changed:
+            controledView?.center = CGPoint(x: (controledView?.center.x)! , y: (controledView?.center.y)! + translation.y)
+            sender.setTranslation(CGPoint.zero, in: view)
+            
+        case .ended:
+            print("end")
+            break
+        default:
+            print("end")
+            break
+        }
+    }
+    
 }
+
