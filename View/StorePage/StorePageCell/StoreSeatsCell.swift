d729d8a10c9a04cc403cc98cc958692f2ee3b193
Leownag <123>
2022-05-11 11:57:25 +0800

    reset merge

---
+++ View/StorePage/StorePageCell/StoreSeatsCell.swift
@@ -0,0 +1,16 @@
+//
+//  StoreSeatsCell.swift
+//  SURU_Leo
+//
+//  Created by LEO W on 2022/5/8.
+//
+
+import UIKit
+
+class StoreSeatsCell: UITableViewCell {
+
+    func layoutCell() {
+        
+    }
+    
+}
