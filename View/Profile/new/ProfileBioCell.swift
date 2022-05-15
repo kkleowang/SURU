4b48ebfba2de6b046e7f8bcc71631ba838439267
Leownag <123>
2022-05-15 16:58:26 +0800

    finish profilePage tableView cell setting.

--- View/Profile/new/ProfileBioCell.swift
+++ View/Profile/new/ProfileBioCell.swift
@@ -6,7 +6,22 @@
 //
 
 import UIKit
+import CHTCollectionViewWaterfallLayout
 
 class ProfileBioCell: UITableViewCell {
 
+    @IBOutlet weak var bioLabel: UILabel!
+    @IBOutlet weak var badgeCollectionView: UICollectionView!
+    
+    var badge: [[Int]]?
+    func layoutCell(bio: String, badgeState: [[Int]]) {
+        bioLabel.text = bio
+        badge = badgeState
+        
+//        badgeCollectionView.dataSource = self
+//        badgeCollectionView.delegate = self
+    }
 }
+//extension ProfileBioCell: UICollectionViewDataSource, UICollectionViewDelegate {
+//
+//}
